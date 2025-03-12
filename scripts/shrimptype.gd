5a1dcc9f236b382c87d6f236003652a16acd252c
Piotrus Watson <piotruswatson@gmail.com>
2025-03-12 19:46:20 +0200

    removed the need for blushing image in shrimptype

--- scripts/shrimptype.gd
+++ scripts/shrimptype.gd
@@ -2,7 +2,6 @@ extends Resource
 class_name ShrimpType
 
 @export var basic_image: Texture2D
-@export var blushing_image: Texture2D
 @export var backgrounds: Array[Texture2D]
 @export var basic_dialogue: DialogueResource
 @export var basic_theme: Theme
