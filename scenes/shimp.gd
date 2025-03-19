55dc398137be05cce493ac088a3e87fb633ff7aa
Piotrus Watson <piotruswatson@gmail.com>
2025-03-20 00:48:19 +0200

    added vision cone! need to make it see the hoke

--- scenes/shimp.gd
+++ scenes/shimp.gd
@@ -2,16 +2,19 @@ extends CharacterBody2D
 
 enum ShimpIs{ROAMING, CHASING, ON_HOOK, CAUGHT}
 
+@onready var sprite = $Sprite2D
 var shimp_status = ShimpIs.ROAMING
 var contained_shrimp: ShrimpType
 @export var speed = 40.0
 var target_direction: Vector2
 var target: Node2D
+
 func _ready():
 	pick_direction()
 	
 func pick_direction():
 	target_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	rotation = target_direction.angle()
 
 func get_direction_to_target():
 	return (target.global_position - global_position).normalized()
@@ -28,3 +31,8 @@ func set_shrimp(shrimp: ShrimpType):
 
 func _on_bored_timer_timeout() -> void:
 	pick_direction()
+
+
+func _on_wall_detector_body_entered(body: Node2D) -> void:
+	if shimp_status == ShimpIs.ROAMING:
+		pick_direction()
