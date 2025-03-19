656125d023d8ae4063415a22f5b9d03338f64ac9
Piotrus Watson <piotruswatson@gmail.com>
2025-03-19 18:55:39 +0200

    made code to push fish hoke

--- scenes/fish_hoke_physics.gd
+++ scenes/fish_hoke_physics.gd
@@ -2,8 +2,13 @@ extends RigidBody2D
 
 signal hooked_shrimp
 signal hook_moved(amount)
+@export var strength = 3.0
 
 @onready var hook = $Hook
 
 func catch_shrimp(shrimp: CharacterBody2D):
 	hook.node_b = shrimp
+
+func push_hook(target: Vector2):
+	var direction = (target - global_position).normalized()
+	apply_central_force(direction * strength)
--- worlds/fishing_zone.gd
+++ worlds/fishing_zone.gd
@@ -1,8 +1,8 @@
-extends CollisionShape2D
+extends Area2D
 
 @export var thing_to_spawn: PackedScene
 @export var shrimp_to_choose: Array[ShrimpType]
-
+@onready var shape = $Shape
 func _ready():
 	if not Globals.first_time:
 		return
@@ -11,7 +11,7 @@ func _ready():
 	Globals.first_time = false
 
 func get_point_in_bounds():
-	var rect = shape.get_rect()
+	var rect = shape.shape.get_rect()
 	var end = rect.end
 	var start = rect.position
 	var x_point = randf_range(start.x, end.x)
@@ -33,3 +33,12 @@ func choose_shrimp():
 func _on_spawn_timer_timeout() -> void:
 	#spawn_thing()
 	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("toggle_fishing"):
+		body.toggle_fishing(true)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.has_method("toggle_fishing"):
+		body.toggle_fishing(false)
