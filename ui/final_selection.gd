4e490bcdc71d5e72209eb55301a18d56bf49070e
Piotrus Watson <piotruswatson@gmail.com>
2025-04-11 01:14:58 +0300

    almost fixed the selection screen

--- ui/final_selection.gd
+++ ui/final_selection.gd
@@ -4,14 +4,15 @@ extends Control
 @export var love_threshold = 0
 @export var slot_scene: PackedScene
 @export var is_debug = false
-@onready var character_box = $Panel/Characters
-
+@onready var character_box = $Panel/ScrollContainer/Characters
+@onready var question_box = $TheQuestion
+var candidate_shrimp: ShrimpType
 func _ready():
 	if is_debug:
 		show_shrimps(debug_shrimps)
 	else:
 		toggle_selection(false)
-
+	toggle_question(false)
 func toggle_selection(is_active: bool):
 	visible = is_active
 	if is_active:
@@ -19,18 +20,41 @@ func toggle_selection(is_active: bool):
 	else:
 		mouse_filter = Control.MOUSE_FILTER_IGNORE
 		
+func toggle_question(is_active: bool):
+	question_box.visible = is_active
+	if is_active:
+		question_box.mouse_filter = Control.MOUSE_FILTER_STOP
+	else:
+		question_box.mouse_filter = Control.MOUSE_FILTER_IGNORE
 func show_shrimps(shrimps: Array[ShrimpType]):
 	for shrimp in shrimps:
 		if shrimp.love > love_threshold:
 			var slot = slot_scene.instantiate()
 			character_box.add_child(slot)
 			slot.fill_deets(shrimp)
+			slot.picked_shrimp.connect(date_shrimp)
 			
 	toggle_selection(true)
 	
-
+func date_shrimp(shrimp: ShrimpType):
+	candidate_shrimp = shrimp
+	toggle_question(true)
 
 func _on_go_alone_pressed() -> void:
+	date_shrimp(null)
 	GlobalMusicPlayer.play_click()
-	Globals.end_status = Enums.EndStatus.ALONE
+
+
+func _on_yes_pressed() -> void:
+	GlobalMusicPlayer.play_click()
+	if candidate_shrimp == null:
+		Globals.end_status = Enums.EndStatus.ALONE
+	else:
+		Globals.end_status = Enums.EndStatus.PICKED
+	Globals.current_shrimp = candidate_shrimp
 	SceneChanger.change_scene(SceneChanger.Worlds.END_SCENE)
+
+
+func _on_no_pressed() -> void:
+	GlobalMusicPlayer.play_click()
+	toggle_question(false)
--- ui/shrimp_slot.gd
+++ ui/shrimp_slot.gd
@@ -4,35 +4,16 @@ extends Control
 @onready var name_text = $ShrimpDeets/Name
 @onready var menu = $Menu
 var shrimp_info: ShrimpType
+signal picked_shrimp(shrimp: ShrimpType)
 
-func _ready() -> void:
-	toggle_menu(false)
 	
 func fill_deets(shrimp: ShrimpType):
 	shrimp_info = shrimp
 	image.texture = shrimp.images.basic
 	name_text.text = shrimp.name
 
-func toggle_menu(is_active):
-	menu.visible = is_active
-	if is_active:
-		menu.mouse_filter = MouseFilter.MOUSE_FILTER_STOP
-	else:
-		menu.mouse_filter = MouseFilter.MOUSE_FILTER_IGNORE
-
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		toggle_menu(true)
-
-
-func _on_no_pressed() -> void:
-	GlobalMusicPlayer.play_click()
-	toggle_menu(false)
-
-
-func _on_yes_pressed() -> void:
-	GlobalMusicPlayer.play_click()
-	Globals.current_shrimp = shrimp_info
-	Globals.end_status = Enums.EndStatus.PICKED
-	SceneChanger.change_scene(SceneChanger.Worlds.END_SCENE)
+		GlobalMusicPlayer.play_click()
+		picked_shrimp.emit(shrimp_info)
